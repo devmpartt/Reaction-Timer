4e4452e943fb62dad697aa259c74ad3434aace0c
devmpartt <79589958+devmpartt@users.noreply.github.com>
2022-05-04 13:41:25 +0300

    new game

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
-  <h1>Reaction Timer</h1>
-
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
+import Block from './components/Block'
+
 export default {
   name: 'App',
-  components: {},
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  methods: {
+    start() {
+      // set time amount (ms)
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+    },
+  }
 }
 </script>
 
@@ -18,5 +34,20 @@ export default {
     text-align: center;
     color: #444;
     margin-top: 60px;
+  }
+  button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
   }
 </style>
