dbac6aeeabfe28db34d669e3945b8507878e3ddc
devmpartt <79589958+devmpartt@users.noreply.github.com>
2022-05-05 18:30:29 +0300

    finishing

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,9 +1,32 @@
 <template>
   <p>Reaction time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
-  props: ['score']
+  props: ['score'],
+  data() {
+    return {
+      rank : null
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Great'
+    } else if (this.score < 400) {
+      this.rank = 'Developable'
+    } else {
+      this.rank = '...weak'
+    }
+  }
 }
 </script>
+
+<style>
+  .rank {
+    font-size: 1.4em;
+    color: #af0f4c;
+    font-weight: bold;
+  }
+</style>
